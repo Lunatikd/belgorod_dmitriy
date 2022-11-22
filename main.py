77900cff8b9f022d99cbda0afc702f652095720b
dmitriy <db.old.stray@gmail.com>
2022-11-22 17:21:12 +0300

    add random circle color

--- main.py
+++ main.py
@@ -16,18 +16,20 @@ class MyForm(QMainWindow):
         self.is_drawing = False
         self.coords = (0, 0)
         self.radius = 0
+        self.color = (0, 0, 0)
 
     def draw_circle(self):
         self.is_drawing = True
         self.coords = (random.randint(20, 300), random.randint(20, 300))
         self.radius = random.randint(2, 60)
+        self.color = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
         self.update()
 
     def paintEvent(self, event):
         if self.is_drawing:
             painter = QPainter()
             painter.begin(self)
-            pen = QtGui.QPen(QColor("yellow"))
+            pen = QtGui.QPen(QColor(*self.color))
             pen.setWidth(5)
             painter.setPen(pen)
 
